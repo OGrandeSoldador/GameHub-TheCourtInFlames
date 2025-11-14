16ea67337488642dea8982f5e4e91e7e973e8e4e
Marcos Guilherme <marcos321francis@gmail.com>
2025-11-14 00:04:27 -0300

    finalizada visual login

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,28 @@
 $(function () {
   const $form = $('#loginForm');
 
-  const username = $('#username').val().trim();
-  const password = $('#password').val().trim();
+  const username = $('#username').val()
+  const password = $('#password').val()
+  console.log('Usuário:', username);
+  console.log('Senha:', password);
+
+  $form.on('keypress', function (e) {
+    $form.removeClass('was-validated');
+  });
 
   $form.on('submit', function (event) {
     if (!$form[0].checkValidity()) {
       event.preventDefault();
       event.stopPropagation();
-      
+
       if (username.length < 3) {
-        const avisoUsuarioCurto = 'Usuário muito curto!';
+        const avisoUsuarioCurto = 'Usuário muito curto, mínimo de 4 caracteres!';
         $('#mensagemErroUsuario').text(avisoUsuarioCurto);
-        $('#username').attr('minlength', 4);
       }
 
-      if (password.length == 0){
-        const avisoSenhaVazia = 'Forneça uma senha!';
-        $('#mensagemErroSenha').text(avisoSenhaVazia);
-        $('#password').val('');
-      } 
-
-      if (password.length < 8){
-        $('#password').attr('minlength', 8);
+      if (password.length < 8) {
+        const avisoSenhaCurta = 'Senha inválida!';
+        $('#mensagemErroSenha').text(avisoSenhaCurta);
         $('#password').val('');
       }
 
@@ -32,6 +32,4 @@ $(function () {
     }
   });
 
-  console.log('Usuário:', username);
-  console.log('Senha:', password);
 });
