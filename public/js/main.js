920b9f679c2605128f177a467770c374bf100a3a
Marcos Guilherme <marcos321francis@gmail.com>
2025-11-17 18:49:15 -0300

    pagina de login acertada

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,2 @@
-$('#botaoCadastro').click(function () {
-    localStorage.setItem('abrirAba', 'cadastro');
-});
-
-$(function () {
-    const aba = localStorage.getItem('abrirAba');
-
-    if (aba === 'cadastro') {
-        const tab = new bootstrap.Tab('#register-tab');
-        tab.show();
-        localStorage.removeItem('abrirAba');
-    }
-});
 
 
