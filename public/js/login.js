8a4f6438e77542383cc721a37b207291e795f298
Marcos Guilherme <marcos321francis@gmail.com>
2025-11-17 18:36:47 -0300

    tratamento para pegar nome de usuario e senha

--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,35 @@
+
+
 $(function () {
-  const $form = $('#loginForm');
+  const form = $('#loginForm');
 
-  const username = $('#username').val()
-  const password = $('#password').val()
-  console.log('Usuário:', username);
-  console.log('Senha:', password);
-
-  $form.on('keypress', function (e) {
-    $form.removeClass('was-validated');
+  form.on('keypress', function (e) {
+    form.removeClass('was-validated');
   });
 
-  $form.on('submit', function (event) {
-    if (!$form[0].checkValidity()) {
+  form.on('submit', function (event) {
+
+    const userName = $('#username').val();
+    const passWord = $('#password').val();
+
+    if (!form[0].checkValidity()) {
       event.preventDefault();
       event.stopPropagation();
 
-      if (username.length < 3) {
+      if (userName.length < 3) {
         const avisoUsuarioCurto = 'Usuário muito curto, mínimo de 4 caracteres!';
         $('#mensagemErroUsuario').text(avisoUsuarioCurto);
       }
 
-      if (password.length < 8) {
+      if (passWord.length < 8 && passWord.length > 32) {
         const avisoSenhaCurta = 'Senha inválida!';
         $('#mensagemErroSenha').text(avisoSenhaCurta);
         $('#password').val('');
       }
 
-      $form.addClass('was-validated');
-      console.log($form.val())
-      return;
+      form.addClass('was-validated');
     }
   });
-
 });
+
+
