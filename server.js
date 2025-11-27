1907910efcc81144b1253fccbc3bef477a49d8a4
ijefiN <jeffersonmecmiler@hotmail.com>
2025-11-26 21:59:44 -0300

    adicionado toastar

--- server.js
+++ server.js
@@ -69,30 +69,34 @@ app.use((req, res, next) => {
       const livereloadPort = process.env.LIVERELOAD_PORT || 35729;
       const isDev = process.env.NODE_ENV === "dev";
 
-      // CSS
       const css = `
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet" />
-            <link rel="stylesheet" href="/css/style.css">
-`;
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet" />
+        <link rel="stylesheet" href="/css/style.css">
 
-      // JS
+        <!-- TOASTR CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+  `;
+
       const js = `
-            <!-- Axios -->
-              <script defer src="/axios/axios.min.js"></script>
-              <script defer src="/js/axios-instance.js"></script>
-            
-            <!-- jQuery e Bootstrap -->
-                  <script defer src="/js/jquery.min.js"></script>
-                  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Axios -->
+        <script defer src="/axios/axios.min.js"></script>
+        <script defer src="/js/axios-instance.js"></script>
+        
+        <!-- jQuery e Bootstrap -->
+        <script defer src="/js/jquery.min.js"></script>
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
+        <!-- TOASTR JS -->
+        <script defer src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 
-            <!-- Livereload -->
-      ${isDev
-          ? `<script defer src="http://localhost:${livereloadPort}/livereload.js?snipver=1"></script>`
-          : ""
+        <!-- Livereload -->
+        ${
+          isDev
+            ? `<script defer src="http://localhost:${livereloadPort}/livereload.js?snipver=1"></script>`
+            : ""
         }
-`;
+  `;
 
       body = body.replace("</head>", `${css}${js}</head>`);
     }
